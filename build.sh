174d857de2f768976354b04ac3aa5c725942546e
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2020-04-20 17:11:33 +1000

    update build file

--- build.sh
+++ build.sh
@@ -9,13 +9,11 @@ echo `g++-9 -v`
 [ -d bin ] && rm -r bin
 mkdir bin
 
-echo "BUILDING THE MetaBCC-LR 15 MER COMPUTATIONS"
+echo "BUILDING THE PlasLR 15 MER COMPUTATIONS"
 g++-9 src/search-15mers.cpp -fopenmp -Wall -o bin/search15mers
-echo "BUILDING THE MetaBCC-LR 3 MER COMPUTATIONS"
+echo "BUILDING THE PlasLR 3 MER COMPUTATIONS"
 g++-9 src/count-tri.cpp -Wall -fopenmp -o bin/countTrimers
-echo "BUILDING THE MetaBCC-LR READ FILTER"
+echo "BUILDING THE PlasLR READ FILTER"
 g++-9 src/filter_reads.cpp -Wall -fopenmp -o bin/filter
-echo "BUILDING READ ASSIGNER"
-g++-9 src/assignBins.cpp -fopenmp -o bin/assign
 
 echo "BUILD FINISHED"
